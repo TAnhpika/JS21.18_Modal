92b56a6083d3e5e6980d7154d63de69c59dcade2
TAnhpika <anhsheeran12345@gmail.com>
2026-04-26 18:31:42 +0700

    21.20 Xử lý để nhiều Modal cùng hoạt động

--- main.js
+++ main.js
@@ -1,35 +1,46 @@
-/** 
-.bind(document) tạo function mới
-Function mới này luôn có this === document
-Khi gọi $('.modal'), this tự động là document
-*/
-
-// chỉ lấy đc phần tử đầu tiên với id --> cải thiện = lấy hết class
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const modal = $("#modal");
+let currentModal = null
 
-$("#open-modal").onclick = function () {
-    modal.classList.add("show");
-};
-
-$("#modal-close").onclick = function () {
-    modal.classList.remove("show");
-};
-
-modal.onclick = function (e) {
-    // sai vì event-bubbling: click thẻ con sẽ nổi bọt ra thẻ cha -> click trong modal vẫn tắt
-    // $('#modal').classList.remove('show')
-
-    if (e.target === modal) {
-        modal.classList.remove("show");
-    }
-};
-
-// thoát khi nhấn Esc
-document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape") {
-        modal.classList.remove("show");
-    }
+$$("[data-modal]").forEach((btn) => {
+    btn.onclick = function () {
+        const modal = $(this.dataset.modal);
+        if (modal) {
+            modal.classList.add("show");
+            currentModal = modal
+        } else {
+            console.error(`${this.dataset.modal} does not exist`);
+        }
+    };
 });
+
+$$(".modal-close").forEach((btn) => {
+    btn.onclick = function () {
+        // const modal = $(this.dataset.modal)
+        const modal = this.closest(".modal-backdrop");
+        modal.classList.remove("show");
+        currentModal = null
+    };
+});
+
+$$(".modal-backdrop").forEach((modal) => {
+    modal.onclick = function (e) {
+        if (e.target === this) {
+            this.classList.remove("show");
+            currentModal = null
+        }
+    };
+});
+
+document.addEventListener('keydown', function(e){
+    if (e.key === 'Escape' && currentModal) {
+        // $$("[data-modal]").forEach(modal => {
+        //     this.classList.remove('show')
+        // })
+
+        currentModal.classList.remove('show')
+        currentModal = null
+        
+    }
+})
